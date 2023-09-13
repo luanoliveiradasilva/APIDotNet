47576012433b3f40a34bd4f90a37a0f643cf8b4f
Luan Oliveira <luanoliveira1107@gmail.com>
2023-09-13 16:05:05 +0100

    Add app

--- src/app/Domains/Entities/User.cs
+++ src/app/Domains/Entities/User.cs
@@ -5,10 +5,10 @@ using System.Threading.Tasks;
 
 namespace src.app.Domains.Entities
 {
-    public class User : BaseEntity
+    public class User: BaseEntity
     {
-        public string Name { get; set; }
+        public string Name {get; set;}
 
-        public string Email { get; set; }
+        public string Email {get; set;}
     }
 }
